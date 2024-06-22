d5fe9eed56e2883870d0d48a980d155a46fb9bb4
Facundo Ríos <Facundorios2005@gmail.com>
2024-06-22 14:30:36 -0300

    feat: Added db conecction

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AdministrationsModule } from './administrations/administrations.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService} from '@nestjs/config'
+
 
 @Module({
-  imports: [AdministrationsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Hace que las variables de entorno sean accesibles en toda la aplicación
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres', 
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // No usar en producción, usar migraciones en su lugar
+      }),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
