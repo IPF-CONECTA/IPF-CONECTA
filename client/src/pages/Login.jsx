de5f2170d1e16ba3e2ba6ec39de8e2ed97e431d6
MeyerHector <meyer-hector@hotmail.com>
2024-07-10 10:53:39 -0300

    WIP: inicio de sesion

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,44 +1,45 @@
 // Login.js
-
+import { useForm } from "react-hook-form";
 import React from "react";
+import axios from "axios";
 import styles from "../../public/css/login.module.css";
 
 export const Login = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Login submitted");
-    // Aquí puedes agregar la lógica para manejar el inicio de sesión
-  };
-
+  const {
+    register,
+    handleSubmit,
+    formState: { values },
+  } = useForm({
+    email: "",
+    password: "",
+  });
+  async function onSubmit(data) {
+    console.log(data);
+    const response = await axios.post(`http://localhost:4000/auth/login`, {
+      user: {
+        email: data.email,
+        password: data.password,
+      },
+    });
+    console.log(response);
+  }
   return (
     <div className={styles["login-container"]}>
       <center>
         <h2>Iniciar Sesión</h2>
       </center>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className={styles["form-group"]}>
           <label htmlFor="email" className={styles.label}>
             Email
           </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className={styles.input}
-            required
-          />
+          <input type="text" required {...register("email")} />
         </div>
         <div className={styles["form-group"]}>
           <label htmlFor="password" className={styles.label}>
             Contraseña
           </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            className={styles.input}
-            required
-          />
+          <input type="password" required {...register("password")} />
         </div>
         <div className={styles["form-group"]}>
           <button type="submit" className={styles.button}>
