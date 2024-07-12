76a8dcd4a4a01cc902b3ca4c2d5242abd745f47c
MeyerHector <meyer-hector@hotmail.com>
2024-07-12 11:22:47 -0300

    Fix: correccion de estilo en el footer

--- client/src/components/AdminCompany.jsx
+++ client/src/components/AdminCompany.jsx
@@ -3,11 +3,17 @@ import { Nav } from "../pages/Nav";
 import { Footer } from "../pages/Footer";
 
 export const AdminCompany = () => {
-    return (
-        <>
-        <Nav />
-        <AdminPanel />
-        <Footer />
-        </>
-    );
-}
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateRows: "auto 1fr auto",
+        minHeight: "100vh",
+      }}
+    >
+      <Nav />
+      <AdminPanel />
+      <Footer />
+    </div>
+  );
+};
