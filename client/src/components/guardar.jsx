9178aeab3f017906d755fa9385b5685776a9967a
MrTex09 <bagente24@gmail.com>
2024-08-08 16:31:25 -0300

    pureba de guardar trabajo

---
+++ client/src/components/guardar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useSavedJobs } from '../context/SavedJobContext';
+
+const JobCard = ({ job }) => {
+  const { saveJob } = useSavedJobs();
+
+  return (
+    <div>
+      <h3>{job.title}</h3>
+      <button onClick={() => saveJob(job)}>Guardar Trabajo</button>
+    </div>
+  );
+};
+
+const SavedJobsPage = () => {
+  const { savedJobs, removeJob } = useSavedJobs();
+
+  return (
+    <div>
+      <h2>Mis Trabajos Guardados</h2>
+      {savedJobs.map((job) => (
+        <div key={job.id}>
+          <h3>{job.title}</h3>
+          <button onClick={() => removeJob(job.id)}>Eliminar</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SavedJobsPage;
