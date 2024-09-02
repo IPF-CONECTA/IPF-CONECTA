a0ef0d3ca45779ffa9a07a2f19f8555754579a75
MeyerHector <meyer-hector@hotmail.com>
2024-09-02 09:59:41 -0300

    style: :lipstick: Cambios para que el footer se posicione bien

--- client/src/components/SupportForm.jsx
+++ client/src/components/SupportForm.jsx
@@ -48,19 +48,21 @@ export const SupportForm = () => {
 
   return (
     <>
-      <main className="d-flex align-items-center">
-        <div className="support-container">
-          <h2 className="support-header">Soporte de Usuarios - IPF-Conecta</h2>
-          <p>
-            Si tienes algún problema o necesitas asistencia, por favor llena el
-            formulario y nuestro equipo de soporte se pondrá en contacto contigo
-            lo antes posible.
-          </p>
-          {error && <p className="error-message">{error}</p>}
-          {successMessage && (
-            <p className="success-message">{successMessage}</p>
-          )}
-          <form onSubmit={handleSubmit}>
+      <main className="d-flex align-items-center mt-3 ">
+        <div className="support-container border">
+          <form onSubmit={handleSubmit} className="border-0 shadow-none p-0">
+            <h2 className="support-header">
+              Soporte de Usuarios - IPF-Conecta
+            </h2>
+            <p>
+              Si tienes algún problema o necesitas asistencia, por favor llena
+              el formulario y nuestro equipo de soporte se pondrá en contacto
+              contigo lo antes posible.
+            </p>
+            {error && <p className="error-message">{error}</p>}
+            {successMessage && (
+              <p className="success-message">{successMessage}</p>
+            )}
             <div className="form-group">
               <label htmlFor="name">Nombre</label>
               <input
@@ -84,7 +86,7 @@ export const SupportForm = () => {
               />
             </div>
             <div className="form-group">
-              <label htmlFor="issue">Descripción del problema</label>
+              <label htmlFor="issue">Mensaje</label>
               <textarea
                 id="issue"
                 name="issue"
