2fcfa2248b999330c2a2ab2874452ca67d347d89
Miqueas <miqueasavalos2004@gmail.com>
2024-11-23 17:13:55 -0300

    Fix loadUser.js

--- server/loadUser.js
+++ server/loadUser.js
@@ -3,28 +3,26 @@ import axios from "axios";
 import path from "path";
 
 // Ruta al archivo JSON
-const filePath = path.resolve("./user.json");
+const filePath = path.resolve("./userData.json");
 
 // Cargar usuarios del archivo JSON
 const loadUsers = async () => {
   try {
-    const data = fs.readFileSync(filePath, "utf-8"); // Leer archivo JSON
+    const data = fs.readFileSync(filePath, "utf-8"); 
     const usersData = JSON.parse(data);
 
-    // Verificar si la propiedad 'users' existe y es un arreglo
     if (!Array.isArray(usersData)) {
       throw new Error("El archivo JSON no contiene un arreglo de usuarios");
     }
 
-    const users = usersData; // Directamente tomar el arreglo de usuarios
+    const users = usersData; 
 
-    console.log("Usuarios cargados:", users); // Mostrar los usuarios para asegurarse de que se cargaron correctamente
-
-    const apiUrl = "http://localhost:4000/create-user"; // Cambia a tu endpoint
+    console.log("Usuarios cargados:", users)
+    const apiUrl = "http://localhost:4000/create-user"; 
 
     for (const userData of users) {
       try {
-        // Envolver el objeto de usuario en la propiedad 'user' como la API espera
+ 
         const response = await axios.post(apiUrl, { user: userData });
         console.log(`Usuario ${userData.username} registrado con éxito.`);
       } catch (error) {
@@ -39,5 +37,4 @@ const loadUsers = async () => {
   }
 };
 
-// Ejecutar la función para cargar usuarios
 loadUsers();
