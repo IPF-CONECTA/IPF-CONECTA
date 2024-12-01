61d753b99134653f45a31dc3521d5d5e1b6a0aea
MeyerHector <meyer-hector@hotmail.com>
2024-12-01 18:55:03 -0300

    style: texto truncado en la tarjeta de cuenta

--- client/src/modules/feed/components/AccountCard.jsx
+++ client/src/modules/feed/components/AccountCard.jsx
@@ -9,48 +9,11 @@ import { authContext } from "../../../context/auth/Context";
 import { useNoti } from "../../../hooks/useNoti";
 
 export const AccountCard = ({ index, account, setOpenConnections }) => {
-  const [showProfile, setShowProfile] = useState(false);
   const noti = useNoti();
   const { authState } = useContext(authContext);
-  const [profile, setProfile] = useState(null);
-  const timeoutRef = useRef(null);
-  const profileRef = useRef(null);
   const [isFollowing, setIsFollowing] = useState(account?.isFollowing);
   const { profileInfo, handleFollowOrUnfollow } = useFollow(account);
   const navigate = useNavigate();
-  const handleShowProfile = (boolean, username) => {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-    if (!boolean) {
-      timeoutRef.current = setTimeout(() => {
-        setShowProfile(false);
-        setProfile(null);
-      }, 200);
-      return;
-    }
-
-    timeoutRef.current = setTimeout(async () => {
-      setShowProfile(true);
-      const res = await getProfileInfo(username);
-      if (res.status !== 200) {
-        return;
-      }
-      setProfile(res.data);
-    }, 500);
-  };
-
-  const handleMouseEnter = () => {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-    setShowProfile(true);
-  };
-
-  const handleMouseLeave = () => {
-    setShowProfile(false);
-    setProfile(null);
-  };
 
   const handleFollowClick = async (event) => {
     if (authState.role == "admin") {
@@ -62,70 +25,52 @@ export const AccountCard = ({ index, account, setOpenConnections }) => {
   };
 
   return (
-    <div
-      className={`avatar w-100 d-flex justify-content-between align-items-center ${
-        index !== 4 && "mb-2"
-      }`}
-      key={account.id}
-    >
-      <div className="d-flex position-relative align-items-center">
-        <img
-          width={40}
-          height={40}
-          className="rounded-circle me-2 border"
-          src={`${BASE_URL}/images/${account.profilePic}`}
-          alt={account.names}
-          onClick={(e) => {
-            e.stopPropagation();
-            setOpenConnections && setOpenConnections(false);
-            navigate(`/perfil/${account.user.username}`);
-          }}
-          onMouseEnter={() => handleShowProfile(true, account.user.username)}
-          onMouseLeave={() => handleShowProfile(false, account.user.username)}
-        />
-        {profile && (
-          <ProfileHover
-            profileData={profile}
-            profileRef={profileRef}
-            handleMouseEnter={handleMouseEnter}
-            handleMouseLeave={handleMouseLeave}
+    <>
+      <div
+        className={`avatar w-100 d-flex bg-white align-items-center ${
+          index !== 4 && "mb-2"
+        }`}
+        key={account.id}
+      >
+        <div className="d-flex position-relative align-items-center w-100 text-truncate">
+          <img
+            width={40}
+            height={40}
+            className="rounded-circle me-2 border"
+            style={{ cursor: "pointer" }}
+            src={`${BASE_URL}/images/${account.profilePic}`}
+            alt={account.names}
+            onClick={(e) => {
+              e.stopPropagation();
+              setOpenConnections && setOpenConnections(false);
+              navigate(`/perfil/${account.user.username}`);
+            }}
           />
-        )}
 
-        <div className=" d-flex flex-column">
-          <span className="fs-6">
-            {account.names.length + account.surnames.length > 15
-              ? account.names.length > 7 && account.surnames.length > 7
-                ? account.names.slice(0, 7) +
-                  "... " +
-                  account.surnames.slice(0, 7) +
-                  "..."
-                : account.names.length > 8
-                ? account.names.slice(0, 8) + " " + account.surnames + "..."
-                : account.surnames.length > 8
-                ? account.names + " " + account.surnames.slice(0, 8) + "..."
-                : account.names + " " + account.surnames
-              : account.names + " " + account.surnames}
-          </span>
-          <span className={`text-muted ${styles.smallText}`}>
-            @{account.user.username}
-          </span>
+          <div className=" d-flex flex-column  w-100">
+            <span className="fs-6 ">
+              {account.names + " " + account.surnames}
+            </span>
+            <span className={`text-muted ${styles.smallText}`}>
+              @{account.user.username}
+            </span>
+          </div>
         </div>
+        {authState?.user?.profile?.id !== account?.id && (
+          <div>
+            <button
+              className={`${styles.buttonFollow} ms-2 ${styles.smallText} ${
+                isFollowing
+                  ? "btn btn-outline-primary text-muted fw-bold p-1"
+                  : "btn btn-primary text-light fw-bold p-1"
+              }`}
+              onClick={handleFollowClick}
+            >
+              {isFollowing ? "Siguiendo" : "Seguir"}
+            </button>
+          </div>
+        )}
       </div>
-      {authState?.user?.profile?.id !== account?.id && (
-        <div>
-          <button
-            className={`${styles.buttonFollow} ms-2 ${styles.smallText} ${
-              isFollowing
-                ? "btn btn-outline-primary text-muted fw-bold p-1"
-                : "btn btn-primary text-light fw-bold p-1"
-            }`}
-            onClick={handleFollowClick}
-          >
-            {isFollowing ? "Siguiendo" : "Seguir"}
-          </button>
-        </div>
-      )}
-    </div>
+    </>
   );
 };
